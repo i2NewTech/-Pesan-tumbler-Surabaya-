d075ff09f5adac40e53412888f86994b06d4ae0f
i2NewTech <104071684+i2NewTech@users.noreply.github.com>
2019-05-05 17:33:21 +0000

    improved file utilities

--- music/demos/ddsp_tone_transfer.ts
+++ music/demos/ddsp_tone_transfer.ts
@@ -10,4 +10,16 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express o
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {AudioFeatures, ModelValues} from '../src/ddsp/interfaces';
+import * as mm from '../src/index';
+import {SPICE} from '../src/index';
+
+enum MODEL {
+  VIOLIN = 'violin',
+  TENOR_SAXOPHONE = 'tenor_saxophone',
+  TRUMPET = 'trumpet
