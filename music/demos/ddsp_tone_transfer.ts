a7667fa38e105164156daa9aebd790b336412799
i2NewTech <104071684+i2NewTech@users.noreply.github.com>
2019-05-09 08:34:46 +0000

    README fixed

--- music/demos/ddsp_tone_transfer.ts
+++ music/demos/ddsp_tone_transfer.ts
@@ -67,4 +67,19 @@ function encodeWAV(samples: Float32Array, sampleRate: number) {
   view.setUint32(24, sampleRate, true);
   /* byte rate (sample rate * block align) */
   view.setUint32(28, sampleRate * 4, true);
-  /* block align (channel count * bytes per 
+  /* block align (channel count * bytes per sample) */
+  view.setUint16(32, channels * 2, true);
+  /* bits per sample */
+  view.setUint16(34, 16, true);
+  /* data chunk identifier */
+  writeString(view, 36, 'data');
+  /* data chunk length */
+  view.setUint32(40, samples.length * 2, true);
+
+  floatTo16BitPCM(view, 44, samples);
+
+  return view;
+}
+
+window.onload = () => {
+  let audioCtx: AudioContext, audioFea
