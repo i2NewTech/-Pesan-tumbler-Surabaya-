5c9ab60ad54f035297fee50011d347bba4ef23ce
i2NewTech <104071684+i2NewTech@users.noreply.github.com>
2019-06-23 17:51:47 +0000

    Update 1.2.0

--- music/demos/metronome.ts
+++ music/demos/metronome.ts
@@ -5,4 +5,12 @@ const metronome =
 const output = document.getElementById('output') as HTMLElement;
 
 document.getElementById('btn').addEventListener('click', (event) => {
-  const btn = event.target as HTMLButtonElem
+  const btn = event.target as HTMLButtonElement;
+  if (metronome.isTicking()) {
+    metronome.stop();
+    btn.textContent = 'start';
+    document.getElementById('controls').removeAttribute('disabled');
+  } else {
+    btn.textContent = 'stop';
+    document.getElementById('output').innerHTML = '';
+    document.getElementById('controls').setAttribute('disabled', 'true')
