0e48d7f6600f3868452aa160e441c6188d247d86
i2NewTech <104071684+i2NewTech@users.noreply.github.com>
2019-10-07 10:22:20 +0000

    po update

--- music/demos/recorder.ts
+++ music/demos/recorder.ts
@@ -3,4 +3,10 @@ import {writeNoteSeqs} from './common';
 
 const recorder = new mm.Recorder();
 const recordBtn = document.getElementById('record') as HTMLButtonElement;
-const stopBtn = document.getElementById('stop') as HTMLButtonEl
+const stopBtn = document.getElementById('stop') as HTMLButtonElement;
+const startStreamBtn =
+    document.getElementById('startStream') as HTMLButtonElement;
+const stopStreamBtnBtn =
+    document.getElementById('stopStream') as HTMLButtonElement;
+const tempoSlider = document.getElementById('tempo') as HTMLInputElement;
+const clickCheckbox = document.getElementById('playClick') as HTMLInputElement;
