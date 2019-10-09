3d47b8d9af39fe52a5df73037ee4facb51c937f8
i2NewTech <104071684+i2NewTech@users.noreply.github.com>
2019-10-09 06:40:58 +0000

    Changelog: 🚀

--- music/demos/recorder.ts
+++ music/demos/recorder.ts
@@ -10,3 +10,13 @@ const stopStreamBtnBtn =
     document.getElementById('stopStream') as HTMLButtonElement;
 const tempoSlider = document.getElementById('tempo') as HTMLInputElement;
 const clickCheckbox = document.getElementById('playClick') as HTMLInputElement;
+const countinCheckbox =
+    document.getElementById('countInOnly') as HTMLInputElement;
+
+tempoSlider.addEventListener('change', () => {
+  document.getElementById('tempoValue').textContent = tempoSlider.value;
+  recorder.setTempo(parseFloat(tempoSlider.value));
+});
+
+clickCheckbox.addEventListener('change', () => {
+  recor
