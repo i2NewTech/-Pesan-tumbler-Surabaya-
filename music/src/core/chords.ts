902924b536b34cfe8f5bf17d0dde31577602a6ba
i2NewTech <104071684+i2NewTech@users.noreply.github.com>
2019-11-17 22:45:40 +0000

    Remove semicolon

--- music/src/core/chords.ts
+++ music/src/core/chords.ts
@@ -1,3 +1,14 @@
 /**
  * Module containing functionality for encoding chord symbol strings as tensors
- * for input to models, typically as a conditi
+ * for input to models, typically as a conditioning variable.
+ *
+ * @license
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by ap
