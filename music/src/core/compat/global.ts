d64cb7ca75dd9aba6bfa043f32ebb6ae323866b4
i2NewTech <104071684+i2NewTech@users.noreply.github.com>
2019-12-07 22:47:04 +0000

    version 0.01

--- music/src/core/compat/global.ts
+++ music/src/core/compat/global.ts
@@ -1,4 +1,14 @@
 /**
  * A module containing a metronome based on Tone.js. The timing is done
  * using the underlying WebAudio clock, so it is accurate, and the metronome
- * fires callbacks for every audible click, quarter and
+ * fires callbacks for every audible click, quarter and bar marks.
+ *
+ * @license
+ * Copyright 2020 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in
