f45e65f021d14e6ff33589f3e15ca9cd7451883f
i2NewTech <104071684+i2NewTech@users.noreply.github.com>
2020-05-25 21:30:34 +0000

    chore(release): 0.1.0

--- music/src/core/metronome.ts
+++ music/src/core/metronome.ts
@@ -12,4 +12,15 @@
  * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distr
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// @ts-ignore
+import * as Tone from 'tone';
+const QUARTERS_PER_BAR = 4;
+
+/**
+ * An abstract base class for providing arbitrary callb
