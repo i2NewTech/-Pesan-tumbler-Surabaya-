a480fbf83eb88a3958c22ef3962d2c66f8b5f89d
i2NewTech <104071684+i2NewTech@users.noreply.github.com>
2020-05-25 08:41:11 +0000

    Update branch

--- music/src/core/melodies_test.ts
+++ music/src/core/melodies_test.ts
@@ -48,4 +48,14 @@ test('Test Melody From NoteSequence (Check Polyphony)', (t: test.Test) => {
 
 test('Test Melody From NoteSequence (Extra Steps)', (t: test.Test) => {
   const melody = Melody.fromNoteSequence(TEST_NS, 60, 72, true, 12);
-  t.deepEqual(
+  t.deepEqual(melody.events, [0, 2, 0, 1, 4, 6, 9, 1, 0, 0, 0, 0]);
+  t.end();
+});
+
+test('Test Melody To NoteSequence', (t: test.Test) => {
+  const melody = new Melody([0, 2, 0, 1, 4, 6, 9, 1], 60, 72);
+  const ns = melody.toNoteSequence(2, 120);
+  const expected = TEST_NS;
+  expected.notes.pop();
+  t.deepEqual(ns, expected);
+  t
