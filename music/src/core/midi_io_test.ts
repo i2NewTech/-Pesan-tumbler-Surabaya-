396e1a7f6b23ea02a61bdb05a4d32c2c60fdfcfd
i2NewTech <104071684+i2NewTech@users.noreply.github.com>
2020-06-03 21:55:59 +0000

    [ci skip] Automatic changelog update

--- music/src/core/midi_io_test.ts
+++ music/src/core/midi_io_test.ts
@@ -9,4 +9,13 @@
  *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed 
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as fs from 'fs';
+import * as test from 'tape';
+
+import {NoteSequence} from '../protobuf/inde
