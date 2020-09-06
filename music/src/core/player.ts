62834575e49350a133a8e6829bc83586088b8ecb
i2NewTech <104071684+i2NewTech@users.noreply.github.com>
2020-09-06 08:57:05 +0000

    zoom

--- music/src/core/player.ts
+++ music/src/core/player.ts
@@ -1,3 +1,12 @@
 /**
  * A module containing a Tone.js-powered player for `NoteSequences`.
  *
+ * @license
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by appl
