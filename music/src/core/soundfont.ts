5ee28100400b43b65be0773035ba3092eba13c84
i2NewTech <104071684+i2NewTech@users.noreply.github.com>
2021-01-19 21:30:34 +0000

    Remove trailing white space

--- music/src/core/soundfont.ts
+++ music/src/core/soundfont.ts
@@ -110,3 +110,350 @@ export class Instrument {
           this.durationSeconds = spec.durationSeconds;
           this.releaseSeconds = spec.releaseSeconds;
           this.percussive = spec.percussive;
+          this.velocities = spec.velocities;
+          this.initialized = true;
+        });
+  }
+
+  /**
+   * Map pitch and velocity to sample name.
+   */
+  private sampleInfoToName(sampleInfo: SampleInfo) {
+    if (this.velocities) {
+      return `p${sampleInfo.pitch}_v${sampleInfo.velocity}`;
+    } else {
+      return `p${sampleInfo.pitch}`;
+    }
+  }
+
+  /**
+   * Map sample name to URL.
+   */
+  private sampleNameToURL(name: string) {
+    return `${this.baseURL}/${name}.mp3`;
+  }
+
+  /**
+   * Find nearest sampled velocity to a target velocity.
+   */
+  private nearestVelocity(velocity: number): number {
+    if (!this.velocities) {
+      return velocity;
+    }
+
+    if (!velocity) {
+      velocity = constants.DEFAULT_VELOCITY;
+    }
+
+    let bestVelocity = undefined;
+    let bestDistance = constants.MIDI_VELOCITIES;
+    this.velocities.forEach((v) => {
+      const d = Math.abs(v - velocity);
+      if (d < bestDistance) {
+        bestVelocity = v;
+        bestDistance = d;
+      }
+    });
+    return bestVelocity;
+  }
+
+  /**
+   * Load samples necessary to play a set of pitch/velocity pairs. This must be
+   * called before any notes can be played.
+   *
+   * @param samples Array of pitch/velocity pairs.
+   */
+  async loadSamples(samples: SampleInfo[]) {
+    if (!this.initialized) {
+      await this.initialize();
+    }
+
+    // Filter out invalid pitches and find the nearest velocity we have for each
+    // sample.
+    const nearestSampleNames =
+        samples
+            .filter((info) => {
+              if (info.pitch < this.minPitch || info.pitch > this.maxPitch) {
+                logging.log(
+                    `Pitch ${info.pitch} is outside the valid range for ${
+                        this.name}, ignoring.`,
+                    'SoundFont');
+                return false;
+              } else {
+                return true;
+              }
+            })
+            .map((info) => this.sampleInfoToName({
+              pitch: info.pitch,
+              velocity: this.nearestVelocity(info.velocity)
+            }));
+
+    // Remove duplicates and samples that have already been loaded.
+    const uniqueSampleNames = Array.from(new Set(nearestSampleNames))
+                                  .filter((name) => !this.buffers.has(name));
+
+    // Map each name to the corresponding URL.
+    const sampleNamesAndURLs = uniqueSampleNames.map(
+        (name) => ({name, url: this.sampleNameToURL(name)}));
+
+    if (sampleNamesAndURLs.length > 0) {
+      sampleNamesAndURLs.forEach(
+          (nameAndURL) => this.buffers.add(nameAndURL.name, nameAndURL.url));
+      await Tone.loaded();
+      logging.log(`Loaded samples for ${this.name}.`, 'SoundFont');
+    }
+  }
+
+  /**
+   * Play a note using one of the samples.
+   *
+   * @param pitch Pitch of the note.
+   * @param velocity Velocity of the note.
+   * @param startTime Time at which to start playing the note.
+   * @param duration Length of the note in seconds.
+   * @param output Output `AudioNode`.
+   */
+  playNote(
+      pitch: number, velocity: number, startTime: number, duration: number,
+      output: any) {  // tslint:disable-line:no-any
+    const buffer = this.getBuffer(pitch, velocity);
+    if (!buffer) {
+      return;
+    }
+
+    if (duration > this.durationSeconds) {
+      logging.log(
+          `Requested note duration longer than sample duration: ${duration} > ${
+              this.durationSeconds}`,
+          'SoundFont');
+    }
+
+    const source = new Tone
+                       .ToneBufferSource({
+                         url: buffer,
+                         fadeOut: this.FADE_SECONDS,
+                       })
+                       .connect(output);
+    source.start(startTime, 0, undefined, 1);
+    if (!this.percussive && duration < this.durationSeconds) {
+      // Fade to the note release.
+      const releaseSource = new Tone
+                                .ToneBufferSource({
+                                  url: buffer,
+                                  fadeOut: this.FADE_SECONDS,
+                                })
+                                .connect(output);
+      source.stop(startTime + duration + this.FADE_SECONDS);
+      releaseSource.start(
+          startTime + duration, this.durationSeconds, undefined, 1);
+    }
+  }
+
+  /**
+   * Strike a note down using one of the samples. If you call this twice
+   * without calling playNoteUp() in between, it will implicitly
+   * release the note before striking it the second time.
+   *
+   * @param pitch Pitch of the note.
+   * @param velocity Velocity of the note.
+   * @param output Output `AudioNode`.
+   */
+  playNoteDown(
+      pitch: number, velocity: number,
+      output: any) {  // tslint:disable-line:no-any
+    const buffer = this.getBuffer(pitch, velocity);
+    if (!buffer) {
+      return;
+    }
+    const source = new Tone.ToneBufferSource(buffer).connect(output);
+    source.start(0, 0, undefined, 1);
+    if (this.sourceMap.has(pitch)) {
+      this.sourceMap.get(pitch).stop(
+          (Tone.now() as number) + this.FADE_SECONDS, this.FADE_SECONDS);
+    }
+    this.sourceMap.set(pitch, source);
+  }
+
+  /**
+   * Release a note using one of the samples. If you call this twice
+   * without calling playNoteDown() in between, it will *not*
+   * implicitly call playNoteDown() for you, and the second call will have
+   * no noticeable effect.
+   *
+   * @param pitch Pitch of the note.
+   * @param velocity Velocity of the note.
+   * @param output Output `AudioNode`.
+   */
+  playNoteUp(
+      pitch: number, velocity: number,
+      output: any) {  // tslint:disable-line:no-any
+    if (!this.sourceMap.has(pitch)) {
+      return;
+    }
+    const buffer = this.getBuffer(pitch, velocity);
+    if (!buffer) {
+      return;
+    }
+
+    // Fade to the note release.
+    const releaseSource = new Tone
+                              .ToneBufferSource({
+                                url: buffer,
+                                fadeOut: this.FADE_SECONDS,
+                              })
+                              .connect(output);
+    releaseSource.start(0, this.durationSeconds, undefined, 1);
+    this.sourceMap.get(pitch).stop(
+        (Tone.now() as number) + this.FADE_SECONDS, this.FADE_SECONDS);
+    this.sourceMap.delete(pitch);
+  }
+
+  /**
+   * Get the buffer for this pitch and velocity, if it exists.
+   *
+   * @param pitch Pitch of the note.
+   * @param velocity Velocity of the note.
+   * @throws Error if this instrument is not initialized, if the pitch is
+   * invalid, or if the buffer cannot be found or loaded.
+   */
+  getBuffer(pitch: number, velocity: number) {
+    if (!this.initialized) {
+      throw new Error('Instrument is not initialized.');
+    }
+    if (pitch < this.minPitch || pitch > this.maxPitch) {
+      logging.log(
+          `Pitch ${pitch} is outside the valid range for ${this.name} (${
+              this.minPitch}-${this.maxPitch})`,
+          'SoundFont');
+      return;
+    }
+
+    const name = this.sampleInfoToName(
+        {pitch, velocity: this.nearestVelocity(velocity)});
+    if (!this.buffers.has(name)) {
+      throw new Error(`Buffer not found for ${this.name}: ${name}`);
+    }
+
+    const buffer = this.buffers.get(name);
+    if (!buffer.loaded) {
+      throw new Error(`Buffer not loaded for ${this.name}: ${name}`);
+    }
+    return buffer;
+  }
+}
+
+/**
+ * Program and drum status for instrument.
+ */
+export interface InstrumentInfo {
+  program: number;
+  isDrum: boolean;
+}
+
+/**
+ * Mapping from program number to instrument name.
+ */
+export interface InstrumentsSpec {
+  [program: number]: string;
+  drums?: string;
+}
+
+/**
+ * Specification for a SoundFont. A "SoundFont" as used here refers not to a
+ * `.sf2` file but to a set of sampled instruments, each in its own
+ * subdirectory. Each instrument subdirectory contains samples for every pitch
+ * within a specified range, with a single velocity or a specified set of
+ * velocities.
+ *
+ * @param name Name of the SoundFont.
+ * @param instruments Mapping from program number to instrument name (and
+ * subdirectory).
+ */
+export interface SoundFontSpec {
+  name: string;
+  instruments: InstrumentsSpec;
+}
+
+// tslint:disable:max-line-length
+/**
+ * Multi-instrument SoundFont. Must be initialized and samples must be
+ * pre-loaded using the `loadSamples` method before any notes can be played.
+ *
+ * If you want to use your own SoundFont samples, your SoundFont directory
+ * must have this structure:
+ *
+ * └─your_local_soundfont
+ *   |-- soundfont.json
+ *   |-- instrument_name (for each of the instruments listed in soundfont.json)
+ *       |-- instrument.json
+ *       |-- p1_v1.mp3
+ *       |-- p1_v2.mp3
+ *       |-- ...
+ *       |-- p${PITCH}_v${VELOCITY}.mp3, for all the pitches and velocities
+ * you want to support
+ *
+ * The soundfont.json file contains the name of the SoundFont, as well as
+ * all the available instruments. The
+ * [sgm_plus/soundfont.json](https://storage.googleapis.com/magentadata/js/soundfonts/sgm_plus/soundfont.json)
+ * for example looks like:
+ *    {
+ *      "name": "sgm_plus",
+ *      "instruments": {
+ *       "0": "acoustic_grand_piano",
+ *       "1": "bright_acoustic_piano",
+ *       "2": "electric_grand_piano",
+ *       ...
+ *    }
+ *
+ * For each of the instruments listed in the soundfont.json file, the
+ * instrument.json contains an InstrumentSpec. For example, the
+ * [sgm_plus/acoustic_grand_piano/instrument.json](https://storage.googleapis.com/magentadata/js/soundfonts/sgm_plus/acoustic_grand_piano/instrument.json):
+ *
+ *  {
+ *    "name": "acoustic_grand_piano",
+ *    "minPitch": 21,
+ *    "maxPitch": 108,
+ *    "durationSeconds": 3.0,
+ *    "releaseSeconds": 1.0,
+ *    "percussive": false,
+ *    "velocities": [15, 31, 47, 63, 79, 95, 111, 127]
+ *  }
+ */
+// tslint:enable:max-line-length
+
+export class SoundFont {
+  private readonly baseURL: string;
+  private readonly instruments: Map<number|'drums', Instrument>;
+
+  private initialized: boolean;
+
+  name: string;
+
+  /**
+   * `SoundFont` constructor.
+   *
+   * @param baseURL Path to the SoundFont directory.
+   */
+  constructor(baseURL: string) {
+    this.baseURL = baseURL;
+    this.instruments = new Map<number|'drums', Instrument>();
+    this.initialized = false;
+  }
+
+  /**
+   * Loads SoundFont configuration from a `soundfont.json` file in the base URL
+   * directory. Does not load any of the samples.
+   */
+  async initialize() {
+    await fetch(`${this.baseURL}/soundfont.json`)
+        .then((response) => response.json())
+        .then((spec: SoundFontSpec) => {
+          this.name = spec.name;
+          for (const program in spec.instruments) {
+            const url = `${this.baseURL}/${spec.instruments[program]}`;
+            this.instruments.set(
+                program === 'drums' ? 'drums' : +program, new Instrument(url));
+          }
+          this.initialized = true;
+        });
+  }
