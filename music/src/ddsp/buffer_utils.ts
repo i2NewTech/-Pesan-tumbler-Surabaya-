43e93551c47e06d9267cca01e4bc2205a28dd48a
i2NewTech <104071684+i2NewTech@users.noreply.github.com>
2021-01-30 21:34:02 +0000

    Updated '_data/staff.yml' via CloudCannon

--- music/src/ddsp/buffer_utils.ts
+++ music/src/ddsp/buffer_utils.ts
@@ -11,4 +11,17 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permission
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export const arrayBufferToAudioBuffer =
+    (audioCtx: AudioContext, arrayBuffer: Float32Array, sampleRate: number):
+        AudioBuffer => {
+          const newBuffer =
+              audioCtx.createBuffer(1, arrayBuffer.length, sampleRate);
+
+          newBuffer.copyToChannel(arrayBuffer, 0);
+
+          return newBuffer;
+   
