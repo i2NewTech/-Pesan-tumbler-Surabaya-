bbf1e5c0cfb267a28bee436e862fe2f744a13b74
i2NewTech <104071684+i2NewTech@users.noreply.github.com>
2021-01-30 13:05:07 +0000

    Update students.yml

--- music/src/ddsp/add_reverb.ts
+++ music/src/ddsp/add_reverb.ts
@@ -23,3 +23,40 @@ export async function addReverb({
   audioCtx,
   arrayBuffer,
   sampleRate,
+}: {audioCtx: AudioContext; arrayBuffer: Float32Array; sampleRate: number;}):
+    Promise<Float32Array> {
+  Tone.setContext(audioCtx);
+  let bufferWithReverb;
+  let resampledAudioBuffer =
+      arrayBufferToAudioBuffer(audioCtx, arrayBuffer, sampleRate);
+  const renderingPromise = Tone.Offline(() => {
+    // prevent sounds from going too loud
+    const limiter = new Tone
+                        .Compressor({
+                          attack: 0.001,
+                          release: 0.001,
+                          threshold: -6,
+                        })
+                        .toDestination();
+    const reverb = new Tone
+                       .Reverb({
+                         wet: 0.3,
+                         decay: 3,
+                       })
+                       .connect(limiter);
+    const filter = new Tone.Filter(8000, 'lowpass', -24).connect(reverb);
+    const player = new Tone
+                       .Player({
+                         url: resampledAudioBuffer,
+                       })
+                       .connect(filter);
+    player.start();
+    return reverb.ready;
+  }, arrayBuffer.length / sampleRate);
+  bufferWithReverb = await renderingPromise;
+  const bufferWithReverbData = bufferWithReverb.getChannelData(0);
+  resampledAudioBuffer = null;
+  bufferWithReverb.dispose();
+  bufferWithReverb = null;
+  return bufferWithReverbData;
+}
