b810cb3bc4c0bcd2df5a4c06ba525b9dca968723
i2NewTech <104071684+i2NewTech@users.noreply.github.com>
2021-02-18 20:09:46 +0000

    add error handling

--- music/src/ddsp/constants.ts
+++ music/src/ddsp/constants.ts
@@ -12,4 +12,17 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * 
+ * limitations under the License.
+ */
+
+export const MIN_VRAM = 50;  // used for memory check
+
+export const LOWEST_LD = -120;
+export const CROSSFADE_DURATION = 1;
+export const OUTPUT_SAMPLE_RATE = 48000;
+
+// Optimal values.
+export const LD_CONF_REDUCTION = -25.0;
+export const CONF_SMOOTH_SIZE = 100;
+export const PITCHES_HOP = 8192;
+export const CONF_THRESHOLD = 0.7;
