ee084cb7a918018e21fc123fa41565d843b7628e
i2NewTech <104071684+i2NewTech@users.noreply.github.com>
2021-02-21 20:34:18 +0000

    MAGETWO-71174: Update doc block information in php classes with @deprecated and @since tags

--- music/src/ddsp/model.ts
+++ music/src/ddsp/model.ts
@@ -61,3 +61,173 @@ class DDSP {
     tf.registerOp('Roll', (node) => {
       const tensors = tf.split(node.inputs[0], 2, 2);
       const result = tf.concat([tensors[1], tensors[0]], 2);
+      tensors.forEach((tensor) => tensor.dispose());
+      return result;
+    });
+    tf.env().set('WEBGL_PACK', false);
+    tf.env().set('WEBGL_CONV_IM2COL', false);
+    tf.env().set('WEBGL_DELETE_TEXTURE_THRESHOLD', 100 * 1024 * 1024);
+
+    this.model = await getModel(`${this.checkpointUrl}/model.json`);
+
+    let settings;
+    try {
+      settings = await fetch(`${this.checkpointUrl}/settings.json`)
+                     .then((res) => res.json());
+    } finally {
+      if (this.settings === null) {
+        throw new Error(
+            'Passing settings is required if you do not have a settings.json file.');
+      }
+    }
+    this.settings = {
+      ...settings,
+      ...this.settings,
+    };
+
+    this.initialized = true;
+  }
+
+  /**
+   * Disposes of any untracked `Tensors` to avoid GPU memory leaks.
+   */
+  dispose() {
+    if (!this.initialized) {
+      return;
+    }
+    this.model.dispose();
+    this.checkpointUrl = null;
+    this.initialized = false;
+  }
+
+  /**
+   * Returns true if model is intialized.
+   */
+  isInitialized() {
+    return this.initialized;
+  }
+
+  /**
+   * Include memCheck as a member method for API/export.
+   *
+   * @param memCheck An AudioBuffer
+   * @returns Audio Features of provided audio buffer
+   */
+  async memCheck(): Promise<boolean> {
+    return await memCheck();
+  }
+
+  /**
+   * Synthesizes audio features based on model.
+   *
+   * @param inputAudioBuffer An AudioBuffer
+   * @returns Audio Features of provided audio buffer
+   */
+  async synthesize(audioFeatures: AudioFeatures, settings?: ModelValues):
+      Promise<Float32Array> {
+    if (settings !== null) {
+      this.settings = {...this.settings, ...settings};
+    }
+
+    const {f0_hz, loudness_db, confidences} = audioFeatures;
+
+    // We need to upsample pitches because spice
+    // gives back something that is smaller.
+    const upsampledPitches = upsample_f0(
+        f0_hz, loudness_db.length, this.settings.modelMaxFrameLength);
+    const upsampledConfidences = upsample_f0(
+        confidences, loudness_db.length, this.settings.modelMaxFrameLength);
+
+    const normalizedAudioFeatures = await normalizeAudioFeatures(
+        {
+          f0_hz: upsampledPitches,
+          loudness_db,
+          confidences: upsampledConfidences,
+        },
+        this.settings);
+
+    // We have to process the audio in chunks according
+    // to the model max frame length.
+    // eg. if we have a 10s input and a 5s model, we'll have
+    // to splice our input into 2, process it twice, and then stitch it
+    // stitching the inputs require a linear mask to prevent an audible blip
+    // we overlap the last second and the first second of 2 conjoining chunks.
+    // eg. for a 10s input [1,2,3,4,5] + [5,6,7,8,9] + [9,10]
+    const audioChunks = [];
+    const inputFrameLength = normalizedAudioFeatures.loudness_db.length;
+    const inputAudioLengthInSecs = convertFrameToSecs(inputFrameLength);
+    let isLastChunks = false;
+    const usableAudioDuration =
+        convertFrameToSecs(this.settings.modelMaxFrameLength);
+    let audioBuffer;
+
+    const mixBufferLength = CROSSFADE_DURATION * MODEL_SAMPLE_RATE;
+    for (let i = 0; i < inputAudioLengthInSecs;
+         i += usableAudioDuration - CROSSFADE_DURATION) {
+      const startFrameIndex = Math.floor(convertSecsToFrame(i));
+      const endFrameIndex = convertSecsToFrame(i + usableAudioDuration);
+      if (endFrameIndex > inputFrameLength) {
+        isLastChunks = true;
+      }
+
+      // tslint:disable-next-line: variable-name
+      const {f0_hz, loudness_db}: AudioFeatures = await resizeAudioFeatures(
+          normalizedAudioFeatures, startFrameIndex, endFrameIndex, isLastChunks,
+          inputFrameLength);
+
+      // tslint:disable-next-line: variable-name
+      const f0hzTensor = tf.tensor1d(f0_hz, 'float32');
+      // tslint:disable-next-line: variable-name
+      const loudnessTensor = tf.tensor1d(loudness_db, 'float32');
+
+      const result = await this.model.predict({
+        f0_hz: f0hzTensor,
+        loudness_db: loudnessTensor,
+      });
+      const resultData = await (result as Tensor).data();
+      audioChunks.push(resultData);
+
+      (result as Tensor).dispose();
+      f0hzTensor.dispose();
+      loudnessTensor.dispose();
+    }
+
+    // Does not require stiching.
+    if (inputAudioLengthInSecs <= usableAudioDuration) {
+      audioBuffer = audioChunks[0];
+    } else {
+      const buffers = [];
+      for (let i = 0; i < audioChunks.length; i++) {
+        const ac = audioChunks[i];
+        buffers.push(ac);
+      }
+      audioBuffer = mixAndJoinAudioData(buffers, mixBufferLength);
+    }
+
+    // Trim off the extra bits.
+    // tslint:disable-next-line: max-line-length
+    const trimmedAudioChannelData: Float32Array =
+        (audioBuffer as Float32Array)
+            .slice(0, audioFeatures.originalRecordedBufferLength);
+
+    // Apply a loudness modifier here to make some
+    // models louder/softer.
+    const trimmedACModified = trimmedAudioChannelData.map(
+        (val) => val * (this.settings.postGain || 1));
+
+    const audioCtx = new Tone.Context();
+    const trimmedAudioBuffer = arrayBufferToAudioBuffer(
+        audioCtx, trimmedACModified, MODEL_SAMPLE_RATE);
+    const resampledAudio =
+        await resampleAndMakeMono(trimmedAudioBuffer, OUTPUT_SAMPLE_RATE);
+    const bufferWithReverbData = await addReverb({
+      audioCtx,
+      arrayBuffer: resampledAudio,
+      sampleRate: OUTPUT_SAMPLE_RATE,
+    });
+
+    return bufferWithReverbData;
+  }
+}
+
+export {DDSP};
