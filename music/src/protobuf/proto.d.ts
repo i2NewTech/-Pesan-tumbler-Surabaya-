4ef6680b1367efa91f6b60f9609bac696d0f9ef7
i2NewTech <104071684+i2NewTech@users.noreply.github.com>
2021-11-19 07:32:07 +0000

    write tests

--- music/src/protobuf/proto.d.ts
+++ music/src/protobuf/proto.d.ts
@@ -192,3 +192,355 @@ export namespace tensorflow {
              * Verifies a NoteSequence message.
              * @param message Plain object to verify
              * @returns `null` if valid, otherwise the reason why it is not
+             */
+            public static verify(message: { [k: string]: any }): (string|null);
+
+            /**
+             * Creates a NoteSequence message from a plain object. Also converts values to their respective internal types.
+             * @param object Plain object
+             * @returns NoteSequence
+             */
+            public static fromObject(object: { [k: string]: any }): tensorflow.magenta.NoteSequence;
+
+            /**
+             * Creates a plain object from a NoteSequence message. Also converts values to other types if specified.
+             * @param message NoteSequence
+             * @param [options] Conversion options
+             * @returns Plain object
+             */
+            public static toObject(message: tensorflow.magenta.NoteSequence, options?: $protobuf.IConversionOptions): { [k: string]: any };
+
+            /**
+             * Converts this NoteSequence to JSON.
+             * @returns JSON object
+             */
+            public toJSON(): { [k: string]: any };
+        }
+
+        namespace NoteSequence {
+
+            /** Properties of a Note. */
+            interface INote {
+
+                /** Note pitch */
+                pitch?: (number|null);
+
+                /** Note pitchName */
+                pitchName?: (tensorflow.magenta.NoteSequence.PitchName|null);
+
+                /** Note velocity */
+                velocity?: (number|null);
+
+                /** Note startTime */
+                startTime?: (number|null);
+
+                /** Note quantizedStartStep */
+                quantizedStartStep?: (number|null);
+
+                /** Note endTime */
+                endTime?: (number|null);
+
+                /** Note quantizedEndStep */
+                quantizedEndStep?: (number|null);
+
+                /** Note numerator */
+                numerator?: (number|null);
+
+                /** Note denominator */
+                denominator?: (number|null);
+
+                /** Note instrument */
+                instrument?: (number|null);
+
+                /** Note program */
+                program?: (number|null);
+
+                /** Note isDrum */
+                isDrum?: (boolean|null);
+
+                /** Note part */
+                part?: (number|null);
+
+                /** Note voice */
+                voice?: (number|null);
+            }
+
+            /** Represents a Note. */
+            class Note implements INote {
+
+                /**
+                 * Constructs a new Note.
+                 * @param [properties] Properties to set
+                 */
+                constructor(properties?: tensorflow.magenta.NoteSequence.INote);
+
+                /** Note pitch. */
+                public pitch: number;
+
+                /** Note pitchName. */
+                public pitchName: tensorflow.magenta.NoteSequence.PitchName;
+
+                /** Note velocity. */
+                public velocity: number;
+
+                /** Note startTime. */
+                public startTime: number;
+
+                /** Note quantizedStartStep. */
+                public quantizedStartStep: number;
+
+                /** Note endTime. */
+                public endTime: number;
+
+                /** Note quantizedEndStep. */
+                public quantizedEndStep: number;
+
+                /** Note numerator. */
+                public numerator: number;
+
+                /** Note denominator. */
+                public denominator: number;
+
+                /** Note instrument. */
+                public instrument: number;
+
+                /** Note program. */
+                public program: number;
+
+                /** Note isDrum. */
+                public isDrum: boolean;
+
+                /** Note part. */
+                public part: number;
+
+                /** Note voice. */
+                public voice: number;
+
+                /**
+                 * Creates a new Note instance using the specified properties.
+                 * @param [properties] Properties to set
+                 * @returns Note instance
+                 */
+                public static create(properties?: tensorflow.magenta.NoteSequence.INote): tensorflow.magenta.NoteSequence.Note;
+
+                /**
+                 * Encodes the specified Note message. Does not implicitly {@link tensorflow.magenta.NoteSequence.Note.verify|verify} messages.
+                 * @param message Note message or plain object to encode
+                 * @param [writer] Writer to encode to
+                 * @returns Writer
+                 */
+                public static encode(message: tensorflow.magenta.NoteSequence.INote, writer?: $protobuf.Writer): $protobuf.Writer;
+
+                /**
+                 * Encodes the specified Note message, length delimited. Does not implicitly {@link tensorflow.magenta.NoteSequence.Note.verify|verify} messages.
+                 * @param message Note message or plain object to encode
+                 * @param [writer] Writer to encode to
+                 * @returns Writer
+                 */
+                public static encodeDelimited(message: tensorflow.magenta.NoteSequence.INote, writer?: $protobuf.Writer): $protobuf.Writer;
+
+                /**
+                 * Decodes a Note message from the specified reader or buffer.
+                 * @param reader Reader or buffer to decode from
+                 * @param [length] Message length if known beforehand
+                 * @returns Note
+                 * @throws {Error} If the payload is not a reader or valid buffer
+                 * @throws {$protobuf.util.ProtocolError} If required fields are missing
+                 */
+                public static decode(reader: ($protobuf.Reader|Uint8Array), length?: number): tensorflow.magenta.NoteSequence.Note;
+
+                /**
+                 * Decodes a Note message from the specified reader or buffer, length delimited.
+                 * @param reader Reader or buffer to decode from
+                 * @returns Note
+                 * @throws {Error} If the payload is not a reader or valid buffer
+                 * @throws {$protobuf.util.ProtocolError} If required fields are missing
+                 */
+                public static decodeDelimited(reader: ($protobuf.Reader|Uint8Array)): tensorflow.magenta.NoteSequence.Note;
+
+                /**
+                 * Verifies a Note message.
+                 * @param message Plain object to verify
+                 * @returns `null` if valid, otherwise the reason why it is not
+                 */
+                public static verify(message: { [k: string]: any }): (string|null);
+
+                /**
+                 * Creates a Note message from a plain object. Also converts values to their respective internal types.
+                 * @param object Plain object
+                 * @returns Note
+                 */
+                public static fromObject(object: { [k: string]: any }): tensorflow.magenta.NoteSequence.Note;
+
+                /**
+                 * Creates a plain object from a Note message. Also converts values to other types if specified.
+                 * @param message Note
+                 * @param [options] Conversion options
+                 * @returns Plain object
+                 */
+                public static toObject(message: tensorflow.magenta.NoteSequence.Note, options?: $protobuf.IConversionOptions): { [k: string]: any };
+
+                /**
+                 * Converts this Note to JSON.
+                 * @returns JSON object
+                 */
+                public toJSON(): { [k: string]: any };
+            }
+
+            /** PitchName enum. */
+            enum PitchName {
+                UNKNOWN_PITCH_NAME = 0,
+                F_FLAT_FLAT = 1,
+                C_FLAT_FLAT = 2,
+                G_FLAT_FLAT = 3,
+                D_FLAT_FLAT = 4,
+                A_FLAT_FLAT = 5,
+                E_FLAT_FLAT = 6,
+                B_FLAT_FLAT = 7,
+                F_FLAT = 8,
+                C_FLAT = 9,
+                G_FLAT = 10,
+                D_FLAT = 11,
+                A_FLAT = 12,
+                E_FLAT = 13,
+                B_FLAT = 14,
+                F = 15,
+                C = 16,
+                G = 17,
+                D = 18,
+                A = 19,
+                E = 20,
+                B = 21,
+                F_SHARP = 22,
+                C_SHARP = 23,
+                G_SHARP = 24,
+                D_SHARP = 25,
+                A_SHARP = 26,
+                E_SHARP = 27,
+                B_SHARP = 28,
+                F_SHARP_SHARP = 29,
+                C_SHARP_SHARP = 30,
+                G_SHARP_SHARP = 31,
+                D_SHARP_SHARP = 32,
+                A_SHARP_SHARP = 33,
+                E_SHARP_SHARP = 34,
+                B_SHARP_SHARP = 35
+            }
+
+            /** Properties of a TimeSignature. */
+            interface ITimeSignature {
+
+                /** TimeSignature time */
+                time?: (number|null);
+
+                /** TimeSignature numerator */
+                numerator?: (number|null);
+
+                /** TimeSignature denominator */
+                denominator?: (number|null);
+            }
+
+            /** Represents a TimeSignature. */
+            class TimeSignature implements ITimeSignature {
+
+                /**
+                 * Constructs a new TimeSignature.
+                 * @param [properties] Properties to set
+                 */
+                constructor(properties?: tensorflow.magenta.NoteSequence.ITimeSignature);
+
+                /** TimeSignature time. */
+                public time: number;
+
+                /** TimeSignature numerator. */
+                public numerator: number;
+
+                /** TimeSignature denominator. */
+                public denominator: number;
+
+                /**
+                 * Creates a new TimeSignature instance using the specified properties.
+                 * @param [properties] Properties to set
+                 * @returns TimeSignature instance
+                 */
+                public static create(properties?: tensorflow.magenta.NoteSequence.ITimeSignature): tensorflow.magenta.NoteSequence.TimeSignature;
+
+                /**
+                 * Encodes the specified TimeSignature message. Does not implicitly {@link tensorflow.magenta.NoteSequence.TimeSignature.verify|verify} messages.
+                 * @param message TimeSignature message or plain object to encode
+                 * @param [writer] Writer to encode to
+                 * @returns Writer
+                 */
+                public static encode(message: tensorflow.magenta.NoteSequence.ITimeSignature, writer?: $protobuf.Writer): $protobuf.Writer;
+
+                /**
+                 * Encodes the specified TimeSignature message, length delimited. Does not implicitly {@link tensorflow.magenta.NoteSequence.TimeSignature.verify|verify} messages.
+                 * @param message TimeSignature message or plain object to encode
+                 * @param [writer] Writer to encode to
+                 * @returns Writer
+                 */
+                public static encodeDelimited(message: tensorflow.magenta.NoteSequence.ITimeSignature, writer?: $protobuf.Writer): $protobuf.Writer;
+
+                /**
+                 * Decodes a TimeSignature message from the specified reader or buffer.
+                 * @param reader Reader or buffer to decode from
+                 * @param [length] Message length if known beforehand
+                 * @returns TimeSignature
+                 * @throws {Error} If the payload is not a reader or valid buffer
+                 * @throws {$protobuf.util.ProtocolError} If required fields are missing
+                 */
+                public static decode(reader: ($protobuf.Reader|Uint8Array), length?: number): tensorflow.magenta.NoteSequence.TimeSignature;
+
+                /**
+                 * Decodes a TimeSignature message from the specified reader or buffer, length delimited.
+                 * @param reader Reader or buffer to decode from
+                 * @returns TimeSignature
+                 * @throws {Error} If the payload is not a reader or valid buffer
+                 * @throws {$protobuf.util.ProtocolError} If required fields are missing
+                 */
+                public static decodeDelimited(reader: ($protobuf.Reader|Uint8Array)): tensorflow.magenta.NoteSequence.TimeSignature;
+
+                /**
+                 * Verifies a TimeSignature message.
+                 * @param message Plain object to verify
+                 * @returns `null` if valid, otherwise the reason why it is not
+                 */
+                public static verify(message: { [k: string]: any }): (string|null);
+
+                /**
+                 * Creates a TimeSignature message from a plain object. Also converts values to their respective internal types.
+                 * @param object Plain object
+                 * @returns TimeSignature
+                 */
+                public static fromObject(object: { [k: string]: any }): tensorflow.magenta.NoteSequence.TimeSignature;
+
+                /**
+                 * Creates a plain object from a TimeSignature message. Also converts values to other types if specified.
+                 * @param message TimeSignature
+                 * @param [options] Conversion options
+                 * @returns Plain object
+                 */
+                public static toObject(message: tensorflow.magenta.NoteSequence.TimeSignature, options?: $protobuf.IConversionOptions): { [k: string]: any };
+
+                /**
+                 * Converts this TimeSignature to JSON.
+                 * @returns JSON object
+                 */
+                public toJSON(): { [k: string]: any };
+            }
+
+            /** Properties of a KeySignature. */
+            interface IKeySignature {
+
+                /** KeySignature time */
+                time?: (number|null);
+
+                /** KeySignature key */
+                key?: (tensorflow.magenta.NoteSequence.KeySignature.Key|null);
+
+                /** KeySignature mode */
+                mode?: (tensorflow.magenta.NoteSequence.KeySignature.Mode|null);
+            }
+
+            /** Represents a KeySignature. */
+            class KeySignature implements IKeySignature {
