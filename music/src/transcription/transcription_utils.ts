95ebb7447bb73057005d0d4bb7bf24bdd7ce29a1
i2NewTech <104071684+i2NewTech@users.noreply.github.com>
2022-01-05 08:16:00 +0000

    Add a few comments

--- music/src/transcription/transcription_utils.ts
+++ music/src/transcription/transcription_utils.ts
@@ -13,4 +13,17 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for th
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Imports
+ */
+import * as tf from '@tensorflow/tfjs';
+
+import {NoteSequence} from '../protobuf/index';
+
+import {FRAME_LENGTH_SECONDS, MIDI_PITCHES, MIN_MIDI_PITCH} from './constants';
+
+// The number of frames of padding needed on each side when splitting into
