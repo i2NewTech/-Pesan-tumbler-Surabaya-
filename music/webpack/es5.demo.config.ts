a712e5fe89776f9472cdcbac3b16afd485e961f2
i2NewTech <104071684+i2NewTech@users.noreply.github.com>
2022-03-06 20:13:57 +0000

    pinctrl: Fix two deadlocks

--- music/webpack/es5.demo.config.ts
+++ music/webpack/es5.demo.config.ts
@@ -2,4 +2,13 @@ import * as fs from 'fs';
 import * as minimist from 'minimist';
 import * as path from 'path';
 
-import {baseConfig} f
+import {baseConfig} from './es5.base.config';
+
+// Allow for specific demos to built with a --demos=<someName>,<someOtherName>
+// CLI format.
+const args = minimist(process.argv.slice(2));
+const specified: string[] = args.demos ? args.demos.split(',') : [];
+
+const getDemos = source => {
+  return fs.readdirSync(source)
+      .filter(name => path.extname(name) =
