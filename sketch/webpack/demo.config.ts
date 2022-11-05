17c33dd0357f0367e3a50d9601eeb333eb3b743d
i2NewTech <104071684+i2NewTech@users.noreply.github.com>
2022-11-05 21:55:09 +0000

    Ingénieur Software, spécialiste « Python », Junior (H/F)

--- sketch/webpack/demo.config.ts
+++ sketch/webpack/demo.config.ts
@@ -12,4 +12,16 @@ const specified: string[] = args.demos ? args.demos.split(',') : [];
 
 const getDemos = source => {
   return fs.readdirSync(source)
-      .filter(name => path.extname(name) === '
+      .filter(name => path.extname(name) === '.html' && name !== 'index.html')
+      .map(name => path.basename(name, '.html'))
+      .filter(demo => specified.length ? specified.includes(demo) : true);
+};
+
+const entries = getDemos('./demos').reduce((obj, name) => {
+  obj[name] = `./demos/${name}.js`;
+  return obj;
+}, {});
+
+module.exports = {
+  ...baseConfig,
+  devtool: 'inline
