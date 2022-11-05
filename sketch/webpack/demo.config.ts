dfd0ecb3adccc1e04652d6633a1265c8a48425db
i2NewTech <104071684+i2NewTech@users.noreply.github.com>
2022-11-05 15:58:58 +0000

    bump cfdot

--- sketch/webpack/demo.config.ts
+++ sketch/webpack/demo.config.ts
@@ -2,4 +2,14 @@ import * as fs from 'fs';
 import * as minimist from 'minimist';
 import * as path from 'path';
 
-import {baseConfig} from './base.conf
+import {baseConfig} from './base.config';
+
+// Allow for specific demos to built with a --demos=<someName>,<someOtherName>
+// CLI format.
+const args = minimist(process.argv.slice(2));
+console.log("-------------", args)
+const specified: string[] = args.demos ? args.demos.split(',') : [];
+
+const getDemos = source => {
+  return fs.readdirSync(source)
+      .filter(name => path.extname(name) === '
